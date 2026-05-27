4a79ae9e174572953c82629b94153837f9b5bb06
Aziz Efe Çırak <azizefe.2508@gmail.com>
2026-05-27 22:23:27 +0300

    docs: add certificate validation links to main.sh header

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Aziz Efe Çırak - 2420191044
 # Sertifika Bağlantıları (3 Adet bağlantı)
-# 
+# https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=lK1hw4qwlo 
+# https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=XV1hWG2yKM
+# https://credsverse.com/credentials/3f0a330b-6466-4bdf-a1cd-b8bb776fe372
 
 echo -n "Lütfen parolayı giriniz: "
 read  PAROLA
